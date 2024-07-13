a1ec68a26674d24e3b8f6e68fdf05cd3693fd9c1
aouiniamine <amine.m.aouini@hotmail.com>
2024-07-13 01:18:48 +0100

    finished update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,18 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+        redirect_to new_user_path
+    else 
+      puts "couldn't update!!"
+    end
+  end
   def user_params
-    
     params.require(:user).permit(:username, :email, :password)
-    
   end
 end
